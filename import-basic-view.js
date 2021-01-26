aeffca7d74318ea21832cba61cf64946dfbf52c6
Cezar Leigos <cezarleigos28@outlook.com>
2021-01-26 20:21:16 -0300

    v0.05 - Adding new components (Adicionando novos componentes)
    English
    
    This time we added some components and changed the colors in the colors.css file to more vivid colors, we also started to define some functions in javascript that we will use to manipulate the components and elements of the framework.
    
    Português
    
    Desta vez adicionamos alguns componentes e mudamos as cores do arquivo colors.css para cores mais vivas, também começamos a definir algumas funções em javascript que usaremos para manipular os componentes e elementos do framework.

---
+++ import-basic-view.js
@@ -0,0 +1,19 @@
+function Message_Popup_Show(){
+
+}
+
+function Message_Popup_Hide(){
+
+}
+
+function Fix_Message_Close(){
+
+}
+
+function Animation_Loop(){
+
+}
+
+function Animation_Count(){
+
+}
