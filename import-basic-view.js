e4afb3cc3c0aab6cac3455a395fc050c679d29d2
Paulo <cezarleigos28@outlook.com>
2021-02-02 14:40:59 -0300

    v0.08 - Adding buttons, forms and other elements (Adicionando botões, formulários e outros elementos)
    [English]
    
    This time, we added some more components to our project, we also made small changes in other parts of the code to adjust some things, the elements added were components like buttons, forms and others. Later, we will change some of these components to better suit our project.
    
    [Português]
    
    Desta vez, adicionamos mais alguns componentes ao nosso projeto, também fizemos pequenas alterações em outras partes do código para ajustar algumas coisas, os elementos adicionados foram componentes como botões, formulários e outros. Posteriormente, alteraremos alguns desses componentes para melhor se adequar ao nosso projeto.

--- import-basic-view.js
+++ import-basic-view.js
@@ -1,19 +1,44 @@
+/*----------------------------------------------------
+here we preload the components and settings of the 
+GridPage elements...
+----------------------------------------------------*/
+function Start(){
+
+}
+
+/*----------------------------------------------------
+Displays a message pop-up  on the screen
+----------------------------------------------------*/
 function Message_Popup_Show(){
 
 }
 
+/*----------------------------------------------------
+hides a message pop-up  on the screen
+----------------------------------------------------*/
 function Message_Popup_Hide(){
 
 }
 
+/*----------------------------------------------------
+Close fixed messages
+----------------------------------------------------*/
 function Fix_Message_Close(){
 
 }
 
-function Animation_Loop(){
+/*----------------------------------------------------
+This function performs loop animation
+----------------------------------------------------*/
+function Animation_Loop(id_document_element){
 
 }
 
-function Animation_Count(){
+/*----------------------------------------------------
+This function performs the animation a number of times
+----------------------------------------------------*/
+function Animation_Count(id_document_element, count){
 
 }
+
+Start(); //code execution
