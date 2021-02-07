d26dae5b364b81152dd306e909d9cc3a400cce75
Cezar Leigos <cezarleigos28@outlook.com>
2021-02-07 18:26:20 -0300

    v0.10 -  Improve the appearance of documentation ( Melhorar a aparência da documentação )
    [English]
    
    This time, we continue to take another look at our documentation to make it more enjoyable, we have also made some occasional changes by adding new features that we will use in the future and removing some that will not be useful in the project for now.
    
    [Português]
    
    Desta vez, continuamos a dar uma outra olhada em nossa documentação para torná-la mais agradável, também fizemos algumas alterações ocasionais adicionando novos recursos que usaremos no futuro e removendo alguns que não serão úteis no projeto por enquanto.

--- import-basic-view.js
+++ import-basic-view.js
@@ -2,42 +2,42 @@
 here we preload the components and settings of the 
 GridPage elements...
 ----------------------------------------------------*/
-function Start(){
+function Gp_Start(){
 
 }
 
 /*----------------------------------------------------
 Displays a message pop-up  on the screen
 ----------------------------------------------------*/
-function Message_Popup_Show(){
+function Gp_Message_Popup_Show(){
 
 }
 
 /*----------------------------------------------------
 hides a message pop-up  on the screen
 ----------------------------------------------------*/
-function Message_Popup_Hide(){
+function Gp_Message_Popup_Hide(){
 
 }
 
 /*----------------------------------------------------
 Close fixed messages
 ----------------------------------------------------*/
-function Fix_Message_Close(){
+function Gp_Fix_Message_Close(){
 
 }
 
 /*----------------------------------------------------
 This function performs loop animation
 ----------------------------------------------------*/
-function Animation_Loop(id_document_element){
+function Gp_Animation_Loop(id_document_element){
 
 }
 
 /*----------------------------------------------------
 This function performs the animation a number of times
 ----------------------------------------------------*/
-function Animation_Count(id_document_element, count){
+function Gp_Animation_Count(id_document_element, count){
 
 }
 
